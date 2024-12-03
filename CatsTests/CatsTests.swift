ee929956da4287b9df41cd280e4c740ed762861f
crichardshen <crichardshen@gmail.com>
2024-12-03 16:25:32 +0800

    Initial Commit

---
+++ CatsTests/CatsTests.swift
@@ -0,0 +1,17 @@
+//
+//  CatsTests.swift
+//  CatsTests
+//
+//  Created by SanRan on 2024/12/3.
+//
+
+import Testing
+@testable import Cats
+
+struct CatsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
